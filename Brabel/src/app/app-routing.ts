a483ef6bbcef30148bab6e42ef4429a699382e0e
Abel <adavid9988@gmail.com>
2022-01-20 19:23:30 -0800

    Router created and routerLink added to SF

---
+++ Brabel/src/app/app-routing.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from "@angular/router";
+
+import {CityComponent} from "./city/city.component";
+
+const routes :Routes = [
+  {
+    path: 'cities/san-francisco',
+    component: CityComponent,
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRouting {}
--- Brabel/src/app/app.module.ts
+++ Brabel/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { CityComponent } from './city/city.component';
 
+import {AppRouting} from "./app-routing";
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -13,6 +16,7 @@ import { CityComponent } from './city/city.component';
   ],
   imports: [
     BrowserModule,
+    AppRouting,
   ],
   providers: [],
   bootstrap: [AppComponent]
