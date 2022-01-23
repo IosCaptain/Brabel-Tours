09775b7a96a9e08c7b1f52304e6331fa95bb1858
Abel <adavid9988@gmail.com>
2022-01-23 15:21:11 -0800

    ngModel added and cards created with loop

--- Brabel/src/app/city/city.component.ts
+++ Brabel/src/app/city/city.component.ts
@@ -6,10 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./city.component.css']
 })
 export class CityComponent implements OnInit {
+  title: string = '';
+  body: string = '';
+  cardsInfo = [
+    {
+      id: 1,
+      title: 'title1',
+      description: 'description1'
+    },
+    {
+      id: 2,
+      title: 'title2',
+      description: 'description2'
+    },
+    {
+      id: 3,
+      title: 'title3',
+      description: 'description3'
+    },
+    {
+      id: 4,
+      title: 'title4',
+      description: 'description4'
+    }
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  createNewCard(title: string, body: string) {
+    console.log(`Title: ${title} and Body: ${body}`)
+
   }
 
 }
